df4e4a06dbfe652bd9604b2446647a3d3ec8c6bf
suzby <skbichl@gmail.com>
2022-05-02 09:52:38 -0700

    sum string of nums

---
+++ sumString.js
@@ -0,0 +1,7 @@
+function sumStr(a,b) {
+    let total = parseInt(a) + parseInt(b)
+    return total.toString()
+  }
+
+  //Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
