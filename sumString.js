ca59601b4be6d9ee68420db48355002ccf1da54c
suzby <skbichl@gmail.com>
2022-05-02 17:16:20 -0700

    complete answer

--- sumString.js
+++ sumString.js
@@ -1,7 +1,16 @@
 function sumStr(a,b) {
+    return String(Number(a) + Number(b))
+    }
+
+
+
+/* returns NaN from an empty string :( 
+
+function sumStr(a,b) {
     let total = parseInt(a) + parseInt(b)
     return total.toString()
   }
 
+*/
   //Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
 
