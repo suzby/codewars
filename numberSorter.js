ba7221c7d78fcbfd6e7f50cf9955f74f78a69581
suzby <skbichl@gmail.com>
2022-05-18 11:08:56 -0700

    added best practice

--- numberSorter.js
+++ numberSorter.js
@@ -7,4 +7,11 @@ function descendingOrder(n) {
    
    /*
    Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+   
+   ********BEST PRACCTICE
+
+   function descendingOrder(n){
+  return parseInt(String(n).split('').sort().reverse().join(''))
+}
+
    */
