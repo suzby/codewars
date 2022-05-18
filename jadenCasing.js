f28f147eeada008f5f709aadbcc9a3d7fd41a42d
suzby <skbichl@gmail.com>
2022-05-18 10:35:42 -0700

    new day new solve

--- jadenCasing.js
+++ jadenCasing.js
@@ -8,6 +8,19 @@ String.prototype.toJadenCase = function () {
   return capitalizedWords.join(' ')
     
   }
+  
+//R2
+
+String.prototype.toJadenCase = function () {
+  let words = this.split(' ')
+  let newArr = []
+  for(let i = 0;i<words.length;i++){
+    newArr.push(words[i][0].toUpperCase() + words[i].slice(1))
+     }
+  return newArr.join(' ')
+};
+
+
 
   /*
   Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
