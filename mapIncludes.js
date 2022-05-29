542cc97c5526a7bb612907acb1f34081a85dc7e7
suzby <skbichl@gmail.com>
2022-05-28 19:47:23 -0700

    map includes

---
+++ mapIncludes.js
@@ -0,0 +1,34 @@
+function printerError(s) {
+    let pass = ['a','b','c','d','e','f','g','h','i','j','k','l','m']
+    let errorLog = 0
+    let splitString = s.split('')
+   
+      splitString.map((x)=>pass.includes(x) === false ? errorLog+=1 : errorLog+=0)
+  
+    return `${errorLog}/${s.length}`
+    
+  }
+
+  /*
+  In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+The string has a length greater or equal to one and contains only letters from ato z.\
+
+BEST 
+function printerError(s) {
+    // your code
+    var count = 0;
+    for(var i = 0; i < s.length; i++) {
+      if (s[i] > "m") {
+        count++;
+      }
+    }
+    return count+"/"+s.length;
+}
+*/
