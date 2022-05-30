a7267035c0d7e6c4cf21f96b18e36858c6560454
suzby <skbichl@gmail.com>
2022-05-30 13:32:04 -0700

    8

---
+++ interiorTriangle.js
@@ -0,0 +1,20 @@
+function otherAngle(a, b) {
+    let c = 180
+    c -= (a+b)
+    return c;
+  }
+  /*
+  You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+https://en.wikipedia.org/wiki/Triangle
+
+BEST
+function otherAngle(a, b) {
+  return 180-(a+b);
+}
+
+*/
