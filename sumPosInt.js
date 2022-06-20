578b7e9bb641789eda326a48160916ee89af3c87
suzby <skbichl@gmail.com>
2022-06-20 11:43:01 -0700

    first try

---
+++ sumPosInt.js
@@ -0,0 +1,17 @@
+function positiveSum(arr) {
+    let total = 0
+    for(let i = 0;i<arr.length;i++){
+      if(arr[i] >= 0){
+        total += arr[i]
+      }
+    }
+    return total
+  }
+
+  /*
+  You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
