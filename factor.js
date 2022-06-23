a8ac73deefe380045cb24109ab116a373b43ce3d
suzby <skbichl@gmail.com>
2022-06-22 21:31:41 -0700

    try to refactor

---
+++ factor.js
@@ -0,0 +1,11 @@
+function checkForFactor (base, factor) {
+    if ( base % factor === 0 ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  function checkForFactor (base, factor) {
+    return base % factor === 0;
+  }
