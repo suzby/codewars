6080a9ddf236c0827426edec627e49ca1f3ac695
suzby <skbichl@gmail.com>
2022-06-29 17:53:09 -0700

    one error

---
+++ compareAgain.js
@@ -0,0 +1,8 @@
+function solution(str, ending){
+    let endingLength = ending.length - 1
+    let toCompare = str.slice(endingLength)
+    return toCompare === ending ? true : false
+  
+  }
+
+  //missing something?? samurai
