2eda3e49ff82cbad66244aa00d0e86968e419bad
suzby <skbichl@gmail.com>
2022-07-01 17:51:51 -0700

    two ways

---
+++ exams.js
@@ -0,0 +1,21 @@
+function finalGrade (exam, projects) {
+    if(exam > 90 || projects > 10){
+      return 100
+    } else if(exam > 75 && projects >= 5){
+      return 90
+    } else if(exam > 50 && projects >= 2){
+      return 75
+    } else{
+    
+    return 0
+     }
+  }
+
+  //or 
+
+  function finalGrade (exam, projects) {
+    if(exam > 90 || projects > 10) return 100;
+    if(exam > 75 & projects >= 5) return 90;
+    if(exam > 50 & projects >= 2) return 75;
+    return 0;
+  }
