9cabaf5e8f3329365ffe0c0bd791a1e8e02b8ee7
suzby <skbichl@gmail.com>
2022-08-06 11:04:36 -0700

    inc optimized

--- twosum.js
+++ twosum.js
@@ -11,4 +11,12 @@ function twoSum(numbers, target) {
       }
      }
     }
-  
+  // optimized
+
+  function twoSum(numbers, target) {
+    for (var i = 0; i < numbers.length-1; i++) {
+        for (var j = i+1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) return [i, j];
+        }
+    }
+}
